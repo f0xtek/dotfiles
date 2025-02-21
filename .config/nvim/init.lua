1b7d67a86ebb5aab8e42f346d713d68ae69a575d
f0xtek <f0xtek@pm.me>
2025-02-21 16:34:14 +0000

    Change keymap for telescope find_files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,5 +2,5 @@ require("config.lazy")
 require("catppuccin").setup()
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
